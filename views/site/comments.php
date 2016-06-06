ee86017b8d8423e9b16a1e70b4f0fa0d860d86ff
vlad <vladislav.groniuk@gmail.com>
2016-06-06 12:40:49 +0300

    Work with URL

--- views/site/comments.php
+++ views/site/comments.php
@@ -6,7 +6,7 @@ use yii\widgets\LinkPager;
 <h1>Comments</h1>
 <ul>
 <?php foreach($comments as $comment): ?>
-<li><b>Name: </b><?=$comment['name']?> <b>Comment: </b><?=$comment['text']?></li>
+<li><b>Name: </b><a href="<?=Yii::$app->urlManager->CreateUrl(['site/user','name'=>$comment->name])?>"><?=$comment['name']?></a> <b>Comment: </b><?=$comment['text']?></li>
 <?php endforeach; ?>
 </ul>
 <?= LinkPager::widget(['pagination'=>$pagination]) ?>
