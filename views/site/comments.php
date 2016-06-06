50b102f2b4c7981edfbc4e1fe2f89aa900910e37
vlad <vladislav.groniuk@gmail.com>
2016-06-06 12:28:15 +0300

    Pagination

--- views/site/comments.php
+++ views/site/comments.php
@@ -1,6 +1,12 @@
+<?php
+use yii\widgets\LinkPager;
+?>
+
+
 <h1>Comments</h1>
 <ul>
 <?php foreach($comments as $comment): ?>
 <li><b>Name: </b><?=$comment['name']?> <b>Comment: </b><?=$comment['text']?></li>
 <?php endforeach; ?>
 </ul>
+<?= LinkPager::widget(['pagination'=>$pagination]) ?>
